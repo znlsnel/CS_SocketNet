e14361f3b9e98bb68b77a0c8882a42915182c7b7
Udi <dnel0605@gmail.com>
2024-03-11 19:34:08 +0900

    #

--- Server/DummyClient/Program.cs
+++ Server/DummyClient/Program.cs
@@ -21,11 +21,11 @@ namespace DummyClient
 			string host = Dns.GetHostName();
 			IPHostEntry ipHost = Dns.GetHostEntry(host);
 			//string Ip6Address = "fe80::e03e:ef3d:d51c:74be%7"; 
-			//string Ip6Address = "192.168.219.200"; 
+			string Ip6Address = "192.168.219.100"; 
 			//string IpAddressLaptop = "192.168.219.101";
 
-			//IPAddress ipAddr = IPAddress.Parse(IpAddressLaptop);
-			IPAddress ipAddr = ipHost.AddressList[0];
+			IPAddress ipAddr = IPAddress.Parse(Ip6Address);
+			//IPAddress ipAddr = ipHost.AddressList[0];
 
 
 			IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
@@ -33,30 +33,18 @@ namespace DummyClient
 			Connector connector = new Connector();
 			//connector.Connect(endPoint, () => { return SessionManager.Instance.Generate(); }, 1);   
 
-			Console.WriteLine("이름을 입력해주세요");
-			string name = "";
-			name = Console.ReadLine();
-
-			connector.Connect(endPoint, () => { return SessionManager.Instance.Generate(name); }, 1);
-
+			connector.Connect(endPoint, () => { return SessionManager.Instance.Generate(); }, 10);
+			 
 			while (true)  
 			{
 				try 
 				{
-					string chatString = Console.ReadLine(); 
-					//connector.Connect(endPoint, () => { return new ServerSession(); });
-					Console.SetCursorPosition(0, Console.CursorTop - 1);
-
-					// 현재 줄에 공백을 출력하여 내용을 지웁니다.
-					Console.Write(new string(' ', Console.WindowWidth));
-					Console.SetCursorPosition(0, Console.CursorTop);
-					SessionManager.Instance.SendForEach(chatString);
-					 
+					SessionManager.Instance.SendForEach();
 				}
 				catch (Exception ex) {
 				    Console.WriteLine(ex);
 				}
-				//	Thread.Sleep(250);
+					Thread.Sleep(250);
 
 
 
--- Server/MainServer/Program.cs
+++ Server/MainServer/Program.cs
@@ -18,7 +18,7 @@ namespace MainServer
 			Room.Push(() => Room.Flush());
 			JobTimer.Instance.Push(FlushRoom, 250);
 		} 
-		 
+		
 		static void Main(string[] args)
 		{
 			Console.WriteLine("===========Server===============");
@@ -27,13 +27,13 @@ namespace MainServer
 			// 172.1.2.3 이러한 IP 주소를 www.naver.com 과 같은 형태로 바꿔줌
 			string host = Dns.GetHostName();
 			//string IpAddress = "fe80::e03e:ef3d:d51c:74be%7";
-		//	string IpAddressPC = "192.168.219.200";
+			string IpAddressPC = "192.168.219.100";
 			//string IpAddressLaptop = "192.168.219.101";
 			 
 			IPHostEntry ipHost = Dns.GetHostEntry(host);
-			IPAddress ipAddr =  ipHost.AddressList[0];
+			//IPAddress ipAddr =  ipHost.AddressList[0];
 
-			//IPAddress ipAddr = IPAddress.Parse(IpAddressLaptop);
+			IPAddress ipAddr = IPAddress.Parse(IpAddressPC);
 			IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 			 
 			_listener.Init(endPoint, () =>{ return SessionManager.Instance.Generate(); });
